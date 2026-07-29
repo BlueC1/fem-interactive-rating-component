f25bf527aa211e817515f2ba7c78a3c9fc0eb1b7
BlueC1 <bluec1@email.com>
2026-07-29 15:57:11 -0500

    finished javascript

---
+++ index.js
@@ -0,0 +1,29 @@
+const ratingCard = document.querySelector('.card-container');
+const thanksCard = document.querySelector('.thanks-container');
+const ratingForm = document.querySelector('.ratings');
+const ratingInput = document.querySelectorAll('input');
+const submitBtn = document.getElementById('submit-btn');
+const ratingText = document.getElementById('rating-text');
+
+let starRating = '';
+
+ratingForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+submitBtn.addEventListener('click', () => {
+  ratingInput.forEach((input) => {
+    if (input.checked) {
+      starRating = input.value;
+    }
+  });
+
+  thanksResults();
+});
+
+function thanksResults() {
+  ratingCard.classList.add('hidden');
+  thanksCard.classList.remove('hidden');
+
+  ratingText.textContent = `You selected ${starRating} out of 5 `;
+}
